80f1da5f1f75e11cb0b252f1b294af1662a0957c
gabychares <gchavez27@alumnos.uaq.mx>
2025-11-16 16:15:44 -0600

    Agregue líneas de código para el análisis de prueba de t

--- 02_Scripts/Desarrollo de funcion.R
+++ 02_Scripts/Desarrollo de funcion.R
@@ -4,24 +4,38 @@
 
 
 maga <- function(){
+  #Preguntas para determinar el analisis estadistico que mejor se acople 
   readline(prompt = "que tipo de variable dependiente estas usando (continua/discreta)?") -> dependiente
   readline(prompt= "que tipo de variable independiente estas usando? (continua/discreta)") -> independiente
   readline (prompt = "¿Estás usando una covariable? (S/N)") -> covariable
   if (dependiente == "continua" & independiente == "discreta"){
-    print("prueba de t, anova o glm")
+    readline (prompt= "¿cantidad de variables dependientes?")-> vnum
+    if(vnum >=2 ){
+      print("anova o glm")
+    }else if (vnum== 1){
+      print ("prueba de t")}
   }else if  (dependiente == "continua" & independiente == "continua"){
     print("glm, correlación o regresión")
   }else if (dependente == "discreta" & independiente == "discreta" ){
     print("tablas de contingencia")
   }
+  # Después se hace el análisis
   readline(prompt = "que analisis vas hacer?") -> analisis
-  if(analisis == "anova"){
-    readline(prompt = "selecciona tu variable dependiente")
-  }
+  if(analisis == "prueba de t"){
+  #Introducir bases de datos
+   readline (prompt = "Una vez precargada tu base de datos en tu Rstudio, introduce tu base de datos ") -> Data 
+   readline (prompt = "Introduce tu variable dependiente") 
+   readline (prompt = "Introduce tu variable independiente") 
+   print ("Ahora introduce en la siguiente función de R tus datos de esta manera:Base de datos$variable dependiente, mu= el promedio de tu hipotesis nula: t.student ()")
+  }else if ( analisis == )
+  #Supuestos de los análisis
+   
+   
+  #Hacer analisis 
 }
 maga()
 
-# Después se hace el análisis
+
 
 
 
