77e87433a684cd948b81b4855b460d29fdecb458
Mariana Gómez Becerra <mgomez110@alumnos.uaq.mx>
2025-11-17 16:47:56 -0600

    Actualicé el README de Scripts

--- 02_Scripts/README_Scripts.R
+++ 02_Scripts/README_Scripts.R
@@ -1,10 +1,17 @@
-# Aquí pondremos las instrucciones de la carpeta de scripts.
 
-# Para el uso de esta función es necesaria que se instalen los paquetes "ggplot2" y "gmodels"
+# La carpeta 02_Scripts es específica para guardar el código utilizado para el análisis de los datos, pero los resultados no se guardan en esta carpeta.
+
+# Para el uso de la función MAGA es necesario que se instalen y carguen los paquetes "ggplot2" y "gmodels"
+
+install.packages ("ggplot2")
+install.packages ("gmodels")
+
+library (ggplot2)
+library (gmodels)
 
 # REFERENCIAS
 
 # 1. Define aesthetic mappings programmatically — aes_. (s. f.). https://ggplot2.tidyverse.org/reference/aes_.html
-# 2. Etzel Garrido.(2023).frijoles"
+# 2. Etzel Garrido.(2023).frijoles
 # 3. Wickham, H. (2025). ggplot2: Create Elegant Data Visualisations Using the Grammar of Graphics (versión 4.0.1). https://github.com/tidyverse/ggplot2
 # 4. Warnes G. R. et al (2024). gmodels: Various R Programming Tools for Model Fitting (Versión 2.19.1). https://github.com/r-gregmisc/gmodels
