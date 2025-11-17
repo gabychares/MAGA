ada8dde5cd10204d685cf5e21b867dc12a79a953
Mariana Gómez Becerra <mgomez110@alumnos.uaq.mx>
2025-11-16 21:21:41 -0600

    Agregué a la función el argumento "data" para facilitar el funcionamiento (data es la base de datos). Arreglé algunos errores gramaticales, y modifiqué las respuestas que da la función. Agregué el código para hacer un anova. Puse al final un ejemplo de uso de la función.

--- 02_Scripts/Desarrollo de funcion.R
+++ 02_Scripts/Desarrollo de funcion.R
@@ -3,45 +3,52 @@
 # que esta estudiando se determine el análisis estadístico que mejor se acople.
 
 
-maga <- function(){
+maga <- function (data) {
   
   # Preguntas para determinar el analisis estadistico que mejor se acople 
   
-  readline(prompt = "que tipo de variable dependiente estas usando (c = continua / d = discreta)?") -> dependiente
-  readline(prompt= "que tipo de variable independiente estas usando? (c = continua / d = discreta)") -> independiente
-  readline (prompt = "¿Estás usando una covariable? (s/n)") -> covariable
+  readline(prompt = "¿Qué tipo de variable dependiente estas usando? (c = continua / d = discreta) ") -> dependiente
+  readline(prompt= "¿Qué tipo de variable independiente estas usando? (c = continua / d = discreta) ") -> independiente
+  readline (prompt = "¿Estás usando una covariable? (s/n) ") -> covariable
 
     if (dependiente == "c" & independiente == "d"){
     readline (prompt= "¿cantidad de variables dependientes?")-> vnum
     if(vnum >=2 ){
-      print("anova o glm")
+      print("Te recomendamos hacer un ANOVA o glm")
     }else if (vnum== 1){
-      print ("prueba de t")}
+      print ("Te recomendamos hacer una prueba de t")}
   }else if  (dependiente == "c" & independiente == "c"){
-    print("glm, correlación o regresión")
+    print("Te recomendamos hacer un glm, correlación o regresión")
   }else if (dependiente == "d" & independiente == "d" ){
-    print("tablas de contingencia")
+    print("Te recomendamos hacer un análisis de tablas de contingencia")
   }
   
   # Después de que se le dan las mejores opciones al usuario, se hace el análisis que este indique
   
   message ("anova = a, ancova = c, glm = g, prueba de t = t, correlación = cr, regresión = r, tablas de contingencia = tc")
-  readline (prompt = "¿Qué analisis vas hacer?") -> analisis
+  readline (prompt = "¿Qué analisis vas hacer? ") -> analisis
   
   if(analisis == "t"){
   #Introducir bases de datos
-   readline (prompt = "Una vez precargada tu base de datos en tu Rstudio, introduce el nombre de tu base de datos ") -> Data 
-   readline (prompt = "Introduce tu variable dependiente, tal como aparece en tu base de datos") 
-   readline (prompt = "Introduce tu variable independiente, tal como aparece en tu base de datos") 
+    
+   readline (prompt = "Introduce tu variable dependiente, tal como aparece en tu base de datos ") 
+   readline (prompt = "Introduce tu variable independiente, tal como aparece en tu base de datos ") 
    print ("Ahora introduce en la siguiente función de R tus datos de esta manera:Base de datos$variable dependiente, mu= el promedio de tu hipotesis nula: t.student ()")
  
    } else if (analisis == "a") {
-    readline (prompt = "Una vez precargada tu base de datos en tu Rstudio, introduce el nombre de tu base de datos ") -> Data 
-    readline (prompt = "Introduce tu variable dependiente, tal como aparece en tu base de datos") -> dep
-    readline (prompt = "Introduce tu variable independiente, tal como aparece en tu base de datos") -> ind
+     
+     readline (prompt = "Introduce el número de columna de tu variable dependiente, tal como aparece en tu base de datos ") -> dep
+     readline (prompt = "Introduce el número de columna de tu variable independiente, tal como aparece en tu base de datos ") -> ind
+     
+     dep <- as.numeric (dep)
+     ind <- as.numeric (ind)
+     
+     anova <- aov (data [, dep] ~ data [ , ind], data)
+     
+     message ("Resultados del análisis:")
     
-    anova <- aov (dep ~ ind, Data)
-    summary (anova)
+     summary (anova)
+     
   }
     
   #Supuestos de los análisis
@@ -50,10 +57,4 @@ maga <- function(){
   #Hacer analisis 
 }
 
-maga()
-
-
-
-
-
-
+maga (ChickWeight) # Ejemplo de uso de la función
