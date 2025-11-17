8b6d44c997ef99e44b7c549e29ac29a227f2bf5d
Mariana Gómez Becerra <mgomez110@alumnos.uaq.mx>
2025-11-17 17:33:25 -0600

    Agregué anotaciones al código y corregí algunas faltas de ortografía. Agregué código en la selección de anova o prueba de t por si el usuario se equivoca. Modifiqué el texto de los supuestos para que sea un poco más claro. Agregué código para verificar el cumplimiento de los supuestos de la regresión lineal. Corregí un error en el último else.

--- 02_Scripts/Desarrollo de funcion.R
+++ 02_Scripts/Desarrollo de funcion.R
@@ -1,14 +1,11 @@
 # Proyecto final 
-# Se tiene como idea que a partir de una función en la cual el usuario introduzca los datos de las variables 
-# que esta estudiando se determine el análisis estadístico que mejor se acople.
 
-library (ggplot2)
-
-library (gmodels)
+# Se creó una función (MAGA) en la cual el usuario puede introducir los datos de las variables 
+# que esta estudiando, y se determina el análisis estadístico que mejor se acople.
 
 maga <- function (data) {
   
-  # Preguntas para determinar el analisis estadistico que mejor se acople 
+  # Preguntas para determinar el análisis estadístico que mejor se acople 
   
   readline(prompt = "¿Qué tipo de variable dependiente estas usando? (c = continua / d = discreta) ") -> dependiente
   readline(prompt= "¿Qué tipo de variable independiente estas usando? (c = continua / d = discreta) ") -> independiente
@@ -18,11 +15,13 @@ maga <- function (data) {
       print("Te recomendamos hacer un ANCOVA")
       
       } else if (dependiente == "c" & independiente == "d"){ # Código para recomendar ANOVA o prueba de t
-        readline (prompt= "¿Cuántas muestras estás comparando?")-> vnum
+        readline (prompt= "¿Cuántas muestras estás comparando?")-> vnum # Permite decidir si se usa ANOVA o prueba de t
         if(vnum >= 2 ){ 
           print("Te recomendamos hacer un ANOVA")
         }else if (vnum == 1){
-            print ("Te recomendamos hacer una prueba de t")}
+            print ("Te recomendamos hacer una prueba de t")} else {
+         print ("Vuelve a intentarlo")
+        }
         
       }else if  (dependiente == "c" & independiente == "c"){ # Código para recomendar regresión lineal
         print("Te recomendamos hacer una regresión lineal")
@@ -30,7 +29,7 @@ maga <- function (data) {
       }else if (dependiente == "d" & independiente == "d" ){ # Código para recomendar tablas de contingencia
         print("Te recomendamos hacer un análisis de tablas de contingencia")
       
-      }
+      } # Fin del código para generar recomendaciones
   
   # Después de que se le dan las mejores opciones al usuario, se hace el análisis que este indique
   
@@ -39,8 +38,10 @@ maga <- function (data) {
   
   if(analisis == "t"){ # Hace la prueba de t
     
-   readline (prompt = "Introduce el número de columna de tu variable dependiente, tal como aparece en tu base de datos ") -> dep 
-   readline (prompt = "Introduce el número de columna de tu variable independiente, tal como aparece en tu base de datos ") -> ind 
+   # En algunos análisis, se optó por utilizar el número de columna de la variable en lugar del nombre, debido a que eso generó los resultados adecuados. En otros casos fue mejor utilizar el nombre en lugar del número de columna.
+   
+   readline (prompt = "Introduce el número de columna de tu variable dependiente, tal como aparece en tu base de datos:  ") -> dep 
+   readline (prompt = "Introduce el número de columna de tu variable independiente, tal como aparece en tu base de datos:  ") -> ind 
    
    dep <- as.numeric (dep) # Esto hace coerción del objeto para que sea considerado numérico, y podamos utilizarlo en la función
    ind <- as.numeric (ind)
@@ -50,8 +51,8 @@ maga <- function (data) {
    
    } else if (analisis == "a") { # Hace el ANOVA y genera las gráficas para verificar el cumplimiento de los supuestos
      
-     readline (prompt = "Introduce el número de columna de tu variable dependiente, tal como aparece en tu base de datos ") -> dep
-     readline (prompt = "Introduce el número de columna de tu variable independiente, tal como aparece en tu base de datos ") -> ind
+     readline (prompt = "Introduce el número de columna de tu variable dependiente, tal como aparece en tu base de datos:  ") -> dep
+     readline (prompt = "Introduce el número de columna de tu variable independiente, tal como aparece en tu base de datos:  ") -> ind
      
      dep <- as.numeric (dep)
      ind <- as.numeric (ind)
@@ -63,37 +64,40 @@ maga <- function (data) {
      summaryanova <- summary (anova) # Muestra los resultados del anova de manera más fácil de visualizar
      print (summaryanova)
      
-     readline (prompt = "Presiona enter para continuar")
+     readline (prompt = "Presiona enter para continuar") # Permite al usuario decidir cuando iniciar el siguiente paso de la función.
      
      print ("A continuación se mostrarán 4 gráficas, que deben cumplir lo siguiente:")
      
      # Muestra los supuestos del anova para que este análisis tenga confiabilidad
      
      message("Residuals vs Fitted y Scale Location permiten observar la homogeneidad de varianzas:")
-     print("Los números que aparecen en la  gráfica a lado de la bolita representa el renglón en el que se encuentra ese residual; Scale Location debería tener pendiente de con tendencia a 0")
+     print("Los números que aparecen en la  gráfica a lado de cada punto representan el renglón en el que se encuentra ese residual; Ambas gráficas deben tener una línea con pendiente con tendencia a 0, y los puntos deben estar bien distribuidos en los ejes X y Y")
      
      message("Con Normal Q-Q  se ve si se cumple el supuesto de la distribución gaussiana de los residuales (distribución normal):")
      print("La línea punteada debe estar de esquina a esquina y cada uno de los residuales debe de caer en la línea punteada o cerca de la línea punteada") 
      
-     message ("Residuals vs Leverage (influencia) de los valores en el análisis o son pseudoréplicas; es para el tercer supuesto (Hay independencia entre los errores y las observaciones):")
+     message ("Residuals vs Leverage: determina la influencia de los valores en el análisis o si hay pseudoréplicas; es para el tercer supuesto (Hay independencia entre los errores y las observaciones):")
      print ("Si no se observa el enunciado de Distancia de Cook: nos representa que no hay un valor que sobresalga en la influencia de los valores, por lo que se cumple el supuesto.")
      
-     # nos da las gráficas por separado
-     #Ver todas las gráficas en un mismo lugar
+     # Primero nos da las gráficas por separado
+     # Y luego muestra todas las gráficas en un mismo plot
+     
      plot(anova)
      layout(matrix(c(1,2,3,4),2,2,))
      plot(anova)
      
-     message ("Si todos los supuestos se cumplen, el resultado de tu análisis es confiable")
+     message ("Si todos los supuestos se cumplen, el resultado de tu análisis es confiable") # Fin del análisis
      
   } else if (analisis == "r") { # Hace el análisis de regresión lineal y genera la gráfica del análisis
+    
     message("Requieres el paquete ggplot2 precargado")
-    readline (prompt = "Introduce el nombre de tu variable dependiente, tal como aparece en tu base de datos ") -> dep
-    readline (prompt = "Introduce el nombre de tu variable independiente, tal como aparece en tu base de datos ") -> ind
     
-    formula <- as.formula (paste (dep, "~", ind)) # Esto genera la fórmula que se utilizará en el análisis de regresión. Es importante hacer la coerción porque la función lm no reconoce el texto obtenido del prompt como un argumento.
+    readline (prompt = "Introduce el nombre de tu variable dependiente, tal como aparece en tu base de datos:  ") -> dep
+    readline (prompt = "Introduce el nombre de tu variable independiente, tal como aparece en tu base de datos:  ") -> ind
     
-    regresion <- lm (formula, data) # lm hace la regresión
+    formula <- as.formula (paste (dep, "~", ind)) # Esto genera la fórmula que se utilizará en el análisis de regresión. Es importante hacer la coerción a fórmula porque la función lm no reconoce el texto obtenido del prompt como un argumento.
+    
+    regresion <- lm (formula, data) # lm hace la regresión, con la fórmula generada en la línea anterior y con el archivo que se agregó en los argumentos.
     
     sreg <- summary (regresion) # Da un resumen estadístico de los resultados del lm
     print (sreg)
@@ -103,29 +107,46 @@ maga <- function (data) {
       geom_point () + # Agrega los puntos de la regresión en el formato predeterminado
       geom_smooth (method = "lm", se = T) # Añade la línea de la regresión
     print (figuraregresion)
+    
     # Supuestos del análisis
-    message("Ahora hay que checar los supuestos:")
-    print ("La varianza en Y es constante, el efecto de x es proporcionalmente igual en cualquier punto de y")
-    print ("Los residuales del modelos (error) se ajustan a una distribución gaussiana")
-   message("Si no se cumple uno se debe buscar otro tipo de análisis")
+    
+    print ("Ahora hay que checar los supuestos:")
+    message ("La varianza en Y es constante, el efecto de x es proporcionalmente igual en cualquier punto de y")
+    message ("Los residuales del modelos (error) se ajustan a una distribución gaussiana")
+    
+    print ("A continuación se mostrarán 3 gráficas, que deben cumplir lo siguiente:")
+    
+    message("Residuals vs Fitted y Scale Location permiten observar la homogeneidad de varianzas:")
+    print("Los números que aparecen en la  gráfica a lado de cada punto representan el renglón en el que se encuentra ese residual; Ambas gráficas deben tener una línea con pendiente con tendencia a 0, y los puntos deben estar bien distribuidos en los ejes X y Y")
+    
+    message("Con Normal Q-Q  se ve si se cumple el supuesto de la distribución gaussiana de los residuales (distribución normal):")
+    print("La línea punteada debe estar de esquina a esquina y cada uno de los residuales debe de caer en la línea punteada o cerca de la línea punteada") 
+    
+    message ("En el ANCOVA, no es necesario que se cumpla el supuesto de pseudoréplicas, ignorar gráfica 4")
+    
+    layout(matrix (c(1,2,3), 1, 3))
+    plot (ancova)
+    
+   message("Si no se cumple uno de los supuestos, los resultados no son confiables y se debe buscar otro tipo de análisis")
     
   } else if (analisis == "tc") { # Hace el análisis de tabla de contingencia
     
     message("Requieres el paquete gmodels precargado")
-    readline (prompt = "Introduce el nombre de tu variable dependiente, tal como aparece en tu base de datos ") -> dep
-    readline (prompt = "Introduce el nombre de tu variable independiente, tal como aparece en tu base de datos ") -> ind
     
-    CrossTable (data [, dep], data [, ind], digits = 5, expected = T, chisq = T)
+    readline (prompt = "Introduce el nombre de tu variable dependiente, tal como aparece en tu base de datos:  ") -> dep
+    readline (prompt = "Introduce el nombre de tu variable independiente, tal como aparece en tu base de datos:  ") -> ind
     
-    figuratc <- ggplot (data, aes (.data [[dep]], fill = .data [[ind]])) +
-      geom_bar (stat = "count", position = "stack")
+    CrossTable (data [, dep], data [, ind], digits = 5, expected = T, chisq = T) # Este análisis es muy sensible a decimales, por lo que se usaron 5. expected y chisq son T para que el resultado muestre los valores esperados y el valor del estadístico chi^2
+    
+    figuratc <- ggplot (data, aes (.data [[dep]], fill = .data [[ind]])) + # La variable dependiente es el eje X, y el eje Y es el conteo de esta. La variable independiente se usa en fill para separar las barras de la variable independiente según la segunda variable.
+      geom_bar (stat = "count", position = "stack") # Indica que el eje Y tiene el conteo, y las barras que se separaron por la variable independiente se apilarán.
     figuratc # Visualización de la gráfica con la tabla de contingencia
   
   } else if (analisis == "c") { # Hace el ANCOVA y genera las gráficas para verificar el cumplimiento de los supuestos
     
-    readline (prompt = "Introduce el número de columna de tu variable dependiente, tal como aparece en tu base de datos ") -> dep
-    readline (prompt = "Introduce el número de columna de tu variable independiente, tal como aparece en tu base de datos ") -> ind
-    readline (prompt = "Introduce el número de columna de tu covariable, tal como aparece en tu base de datos ") -> cov
+    readline (prompt = "Introduce el número de columna de tu variable dependiente, tal como aparece en tu base de datos:  ") -> dep
+    readline (prompt = "Introduce el número de columna de tu variable independiente, tal como aparece en tu base de datos:  ") -> ind
+    readline (prompt = "Introduce el número de columna de tu covariable, tal como aparece en tu base de datos:  ") -> cov
     
     dep <- as.numeric (dep)
     ind <- as.numeric (ind)
@@ -143,7 +164,7 @@ maga <- function (data) {
     print ("A continuación se mostrarán 4 gráficas, que deben cumplir lo siguiente:")
     
     message("Residuals vs Fitted y Scale Location permiten observar la homogeneidad de varianzas:")
-    print("Los números que aparecen en la  gráfica a lado de la bolita representa el renglón en el que se encuentra ese residual; Scale Location debería tener pendiente de con tendencia a 0")
+    print("Los números que aparecen en la  gráfica a lado de cada punto representan el renglón en el que se encuentra ese residual; Ambas gráficas deben tener una línea con pendiente con tendencia a 0, y los puntos deben estar bien distribuidos en los ejes X y Y")
     
     message("Con Normal Q-Q  se ve si se cumple el supuesto de la distribución gaussiana de los residuales (distribución normal):")
     print("La línea punteada debe estar de esquina a esquina y cada uno de los residuales debe de caer en la línea punteada o cerca de la línea punteada") 
@@ -151,22 +172,24 @@ maga <- function (data) {
     message ("Residuals vs Leverage (influencia) de los valores en el análisis o son pseudoréplicas; es para el tercer supuesto (Hay independencia entre los errores y las observaciones):")
     print ("Si no se observa el enunciado de Distancia de Cook: nos representa que no hay un valor que sobresalga en la influencia de los valores, por lo que se cumple el supuesto.")
     
-    # nos da las gráficas por separado
-    #Ver todas las gráficas en un mismo lugar
+    # Nos da las gráficas por separado
+    # Y después muestra todas las gráficas en un mismo plot
+    
     plot(ancova)
     layout(matrix(c(1,2,3,4),2,2,))
     plot(ancova)
     
     message ("Por último, se debe cumplir el supuesto: Los factores no afectan a la covariable. Esto se verifica con un anova de una vía. Si el valor de p es mayor a 0.05, el supuesto se cumple")
     
-    supuesto4 <- aov (data [, cov] ~ data [ , ind], data) 
+    supuesto4 <- aov (data [, cov] ~ data [ , ind], data) # Analiza si la variable independiente tiene un efecto sobre la variación en la covariable
     summarysup4 <- summary (supuesto4)
     print (summarysup4)
     
     message ("Si todos los supuestos se cumplen, el resultado de tu análisis es confiable")
     
-  }
-  else ("Vuelve a cargar la función para introducir nuevamente tus datos") # Esta línea permite al usuario finalizar la operación en caso de haber cometido un error al introducir los datos
+  }  else {
+  print ("Lo sentimos, no tenemos disponible ese análisis. Vuelve a cargar la función para reintroducir tus datos si crees que esto es un error.")
+    } # Esta línea permite al usuario finalizar la operación en caso de haber cometido un error al introducir los datos
 }
 
-maga (frijoles) # Ejemplo de uso de la función
+maga () # Como argumento debe ponerse el nombre de la base de datos, previamente asignada a un objeto en R.
