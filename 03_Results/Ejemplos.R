cff3add3907b113915f5952cddfba9ca4e6dad67
gabychares <gchavez27@alumnos.uaq.mx>
2025-11-17 16:15:13 -0600

    Agregué la cita de la base de datos y en donde se encuentran las bases de datos

--- 03_Results/Ejemplos.R
+++ 03_Results/Ejemplos.R
@@ -1,6 +1,7 @@
 
-#Precargar la base de datos para conocer las variables
+# Precargar la base de datos para conocer las variables
 maga (ChickWeight) # Ejemplo de uso de la función para hacer un anova
 maga (biostat) # Ejemplo de uso de la función para hacer una prueba de t
 maga (Orange) # Ejemplo de uso de la función para hacer una regresión lineal
-maga (frijoles) # Ejemplo de uso de la función para ancova y tablas de contingencia
+maga (frijoles) # Ejemplo de uso de la función para ancova y tablas de contingencia, (Garrido, 2023). 
+# Estas bases de datos se encuentran en la carpeta de RawData 
