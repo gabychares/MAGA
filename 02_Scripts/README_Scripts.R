b00ebb20d07d2c38e202d89027edc0df33310f95
gabychares <gchavez27@alumnos.uaq.mx>
2025-11-17 16:20:59 -0600

    Agregué los paquetes que se ocupan para usar la función y referencias de estos

--- 02_Scripts/README_Scripts.R
+++ 02_Scripts/README_Scripts.R
@@ -1,5 +1,10 @@
 # Aquí pondremos las instrucciones de la carpeta de scripts.
 
+# Para el uso de esta función es necesaria que se instalen los paquetes "ggplot2" y "gmodels"
+
 # REFERENCIAS
 
 # 1. Define aesthetic mappings programmatically — aes_. (s. f.). https://ggplot2.tidyverse.org/reference/aes_.html
+# 2. Etzel Garrido.(2023).frijoles"
+# 3. Wickham, H. (2025). ggplot2: Create Elegant Data Visualisations Using the Grammar of Graphics (versión 4.0.1). https://github.com/tidyverse/ggplot2
+# 4. Warnes G. R. et al (2024). gmodels: Various R Programming Tools for Model Fitting (Versión 2.19.1). https://github.com/r-gregmisc/gmodels
